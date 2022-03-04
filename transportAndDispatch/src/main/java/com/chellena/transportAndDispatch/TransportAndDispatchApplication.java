12e47a4ae95fb239daf7819b4446db6a3801f84c
GodifGit <tgiorgis12@yahoo.com>
2022-03-03 23:21:40 -0500

    Base Project Backend

---
+++ transportAndDispatch/src/main/java/com/chellena/transportAndDispatch/TransportAndDispatchApplication.java
@@ -0,0 +1,13 @@
+package com.chellena.transportAndDispatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportAndDispatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportAndDispatchApplication.class, args);
+	}
+
+}
---
+++ transportAndDispatch/src/test/java/com/chellena/transportAndDispatch/TransportAndDispatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chellena.transportAndDispatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportAndDispatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
